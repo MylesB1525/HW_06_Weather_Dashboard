2e27da77338af37b1babbe6952a292a4f9f8a913
Myles Burkemper <myles.burkemper@gmail.com>
2021-02-22 11:06:10 -0600

    fixed forecast call, local storage, and added README.

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# HW_06_Weather_Dashboard
+# HW_06_Weather-Dashboard
+
+## Description
+
+A JavaScript and HTML based weather application that uses OpenWeatherMapAPI to show the current tempeture and forcast for a user selected city.
+
+### Table of Contents
+
+-[Installation](#installation) -[Usage](#usage) -[Contributing](#contributing) -[Questions](#questions)
+
+## Installation
+
+To install the necessary dependencies for this project, use the command none.
+
+## Usage
+
+Search for a city to see info about tempature, wind, percipitation, and UV index.
+
+## Contributing
+
+n/a
+
+## Questions
+
+If you have questions about this repository, contact MylesB1525 at https://github.com/MylesB1525
+or email at myles.burkemper@gmail.com.
--- script.js
+++ script.js
@@ -8,6 +8,10 @@ var temp = document.getElementById("temperature");
 var uv = document.getElementById("UV-index");
 var forecast = document.getElementById("forecast");
 var apiKey = "66692d4c89b03e908fbb333c27dab470";
+var cities = [];
+if (localStorage.getItem("city")) {
+  cities = JSON.parse(localStorage.getItem("city"));
+}
 
 $(document).ready(function () {
   // function for current weather
@@ -48,22 +52,24 @@ $(document).ready(function () {
   // Forecast funtion (Need to fix Forcast, Icons, Local Storage)
   function getForecast(cityName) {
     let queryURL =
-      "api.openweathermap.org/data/2.5/forecast?id=" +
+      "http://api.openweathermap.org/data/2.5/forecast?q=" +
       cityName +
       "&appid=" +
       apiKey;
+    console.log(queryURL);
 
     $.get(queryURL).then(function (response) {
       let forecastInfo = response.list;
-      forecastDiv.empty();
+      $(".fiveday").empty();
       $.each(forecastInfo, function (i) {
+        console.log(moment(forecastInfo[i].dt_txt).format("l"));
         if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
           return;
         }
         let forecastDate = new Date(forecastInfo[i].dt * 1000);
         let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
 
-        forecastDiv.append(`
+        $(".fiveday").append(`
           <div class="col-md">
             <div class="card text-white bg-primary">
               <div class="card-body">
@@ -88,5 +94,33 @@ $(document).ready(function () {
       var city = document.querySelector("#inputBar").value.trim();
       getWeather(city);
       getForecast(city);
+      cities.push(city);
+      localStorage.setItem("city", JSON.stringify(cities));
+      renderButtons();
     });
+
+  function renderButtons() {
+    $("#buttons-view").empty();
+
+    for (var i = 0; i < cities.length; i++) {
+      var a = $("<button>");
+      a.addClass("city");
+      a.attr("data-name", cities[i]);
+      a.text(cities[i]);
+      // Adding the button to the HTML
+      $("#buttons-view").append(a);
+    }
+  }
+
+  function displaycityInfo() {
+    var city = $(this).attr("data-name");
+    getForecast(city);
+    getWeather(city);
+  }
+
+  $(document).on("click", ".city", displaycityInfo);
+
+  renderButtons();
+  getForecast("Kansas City");
+  getWeather("Kansas City");
 });
